3cfc9f4832b941353377705937c85f3059918fa5
lazypool <lazypool@proton.me>
2026-03-07 14:23:30 +0800

    feat: synchronize the baselines and policy gradients between proc

--- forward_only/policy_gradient_optimizer.py
+++ forward_only/policy_gradient_optimizer.py
@@ -8,12 +8,12 @@
 # This code is based on SpinQuant(https://github.com/faceresearch/SpinQuant/tree/main/SpinQuant).
 # Licensed under Apache License 2.0.
 
-from forward_only.rotation_model import RotateModule
 import torch
+import torch.distributed as dist
 from torch.optim.optimizer import Optimizer
 
 class PolicyGradientOptimizer(Optimizer):
-    def __init__(self, params, modules, lr=1e-3, T=5, N=100) -> None:
+    def __init__(self, params, modules, lr=1e-3, T=5, N=10) -> None:
         defaults = dict(lr=lr)
         super().__init__(params, defaults)
         self.lr, self.T, self.N = lr, T, N
@@ -43,12 +43,16 @@ class PolicyGradientOptimizer(Optimizer):
         if len(self.samples) == N:
             # calculate average loss
             avg_loss = sum(sample['loss'] for sample in self.samples) / N
+            if dist.is_available() and dist.is_initialized():
+                avg_loss_ = torch.tensor(avg_loss, device='cuda')
+                dist.all_reduce(avg_loss_, op=dist.ReduceOp.AVG)
+                avg_loss = avg_loss_.item()
 
             # update baseline
             self.baseline = (T - 1) / T * self.baseline + (1 / T) * avg_loss
 
             # calculate average grad with baseline
-            avg_grads = [[0.0, 0.0] for _ in range(0, len(self.rotate_modules))]
+            avg_grads = [[0.0, 0.0] for _ in range(len(self.rotate_modules))]
             for sample in self.samples:
                 loss_ = sample['loss'] - self.baseline
                 for i, grad in enumerate(sample['grads']):
@@ -57,6 +61,14 @@ class PolicyGradientOptimizer(Optimizer):
             for i in range(0, len(self.rotate_modules)):
                 avg_grads[i][0] /= N
                 avg_grads[i][1] /= N
+            if dist.is_available() and dist.is_initialized():
+                for i in range(len(self.rotate_modules)):
+                    mu_grad_ = avg_grads[i][0].cuda()
+                    rho_grad_ = avg_grads[i][1].cuda()
+                    dist.all_reduce(mu_grad_, op=dist.ReduceOp.AVG)
+                    dist.all_reduce(rho_grad_, op=dist.ReduceOp.AVG)
+                    avg_grads[i][0] = mu_grad_
+                    avg_grads[i][1] = rho_grad_
 
             # update params
             for module, grad in zip(self.rotate_modules, avg_grads):
